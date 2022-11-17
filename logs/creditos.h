c74e60f62aa2a26adac018e312fde08df79a37d3
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-11-17 17:11:35 -0300

    Update creditos.h

--- logs/creditos.h
+++ logs/creditos.h
@@ -1,31 +1,31 @@
 #include <stdio.h>
 int creditos(){
-     printf("/*****************************************************************************|\n");
-     printf("/                 ALUNO: Thiago Germano                                       |\n");
-     printf("/                   git: https://github.com/tgn76ok                           |\n");
-     printf("/linkedin:https://www.linkedin.com/in/thiago-germano-do-nascimento-a66624233/ |\n");
-     printf("/             instragam:                                                      |\n");
-     printf("/*****************************************************************************|\n");
+     printf("/*******************************************************************************************|\n");
+     printf("/                 ALUNO: Thiago Germano                                                     |\n");
+     printf("/                   git: https://github.com/tgn76ok                           		 |\n");
+     printf("/              linkedin: https://www.linkedin.com/in/thiago-germano-do-nascimento-a66624233/|\n");
+     printf("/             instragam: @thiago.tgn                                                        |\n");
+     printf("/*******************************************************************************************|\n");
 
-     printf("/******************************************************|\n");
-     printf("/                 ALUNO: Luan Victor                   |\n");
-     printf("/                   git: https://github.com/luanxvsm   |\n");
-     printf("/              linkedin:                               |\n");
-     printf("/             instragam:                               |\n");
-     printf("/******************************************************|\n");
+     printf("/*******************************************************************************************|\n");
+     printf("/                 ALUNO: Luan Victor                                                        |\n");
+     printf("/                   git: https://github.com/luanxvsm                                        |\n");
+     printf("/              linkedin: https://www.linkedin.com/in/luan-victor-30aa26235/                 |\n");
+     printf("/             instragam: @luanvictorsm                                                      |\n");
+     printf("/*******************************************************************************************|\n");
 
-     printf("/******************************************************|\n");
-     printf("/                 ALUNO: Murilo Henrique               |\n");
-     printf("/                   git: https://github.com/MuriloHSav |\n");
-     printf("/              linkedin:                               |\n");
-     printf("/             instragam:                               |\n");
-     printf("/******************************************************|\n");
+     printf("/*******************************************************************************************|\n");
+     printf("/                 ALUNO: Murilo Henrique                                                    |\n");
+     printf("/                   git: https://github.com/MuriloHSav                                      |\n");
+     printf("/              linkedin: https://www.linkedin.com/in/murilo-henrique-78aa4623a/             |\n");
+     printf("/             instragam: @_murilo.sav                                                       |\n");
+     printf("/*******************************************************************************************|\n");
 
-     printf("/******************************************************|\n");
-     printf("/                 ALUNO: Gabriel FIgueiredo            |\n");
-     printf("/                   git: https://github.com/gouacamole |\n");
-     printf("/              linkedin:                               |\n");
-     printf("/             instragam:                               |\n");
-     printf("/******************************************************|\n");
+     printf("/*******************************************************************************************|\n");
+     printf("/                 ALUNO: Gabriel FIgueiredo                                                 |\n");
+     printf("/                   git: https://github.com/gouacamole                                      |\n");
+     printf("/              linkedin: https://www.linkedin.com/in/gabriel-figueiredo-413325219/          |\n");
+     printf("/             instragam: @gabrielfgd_                                                       |\n");
+     printf("/*******************************************************************************************|\n");
 
 }
